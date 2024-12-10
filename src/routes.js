e78ab4b2fba9882ba1fa4ec6e68bf7364797fab3
devanify <yanevan03@gmail.com>
2024-12-11 00:45:15 +0800

    feat/route

---
+++ src/routes.js
@@ -0,0 +1,33 @@
+const {
+  simpanBukuHandler, tampilSemuaBukuHandler, detailBukuHandler, editBukuHandler, hapusBukuHandler,
+} = require('./handler.js');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: simpanBukuHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: tampilSemuaBukuHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: detailBukuHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: editBukuHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: hapusBukuHandler,
+  },
+];
+
+module.exports = routes;
